49952412b459273937fb2b84a6c0e2240c859cca
tsigewossne <tsigedejen@gmail.com>
2022-09-19 15:18:45 +0300

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,10 +4,12 @@
 
 class Rectangle:
     """Representation of a rectangle"""
+
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.height = height
         self.width = width
+
         @property
         def width(self):
             """getter for the private instance attribute width"""
@@ -21,6 +23,7 @@ class Rectangle:
             if value < 0:
                 raise ValueError("width must be >= 0")
             self.__width = value
+
             @property
             def height(self):
                 """getter for the private instance attribute height"""
