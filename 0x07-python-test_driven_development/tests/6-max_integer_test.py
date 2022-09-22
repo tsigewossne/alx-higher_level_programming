ebdf49b6655a5d160fb83a5ae8b63600c3b76c46
tsigewossne <tsigedejen@gmail.com>
2022-09-22 15:33:42 +0300

    6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,12 +6,12 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-        def test_max(self):
-                    self.assertAlmostEqual(max_integer([4, 5, 7]), 7)
-                            self.assertAlmostEqual(max_integer([8, 234, 332, 33]), 332)
-                                    self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
-                                            self.assertAlmostEqual(max_integer([20000000022]), 20000000022)
-                                                    self.assertAlmostEqual(max_integer([]), None)
+            def test_max(self):
+                            self.assertAlmostEqual(max_integer([4, 5, 7]), 7)
+                                    self.assertAlmostEqual(max_integer([8, 234, 332, 33]), 332)
+                                            self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
+                                                    self.assertAlmostEqual(max_integer([20000000022]), 20000000022)
+                                                            self.assertAlmostEqual(max_integer([]), None)
 
-                                                        def test_non_int(self):
-                                                                    self.assertRaises(TypeError, max_integer, True)
+                                                                def test_non_int(self):
+                                                                                self.assertRaises(TypeError, max_integer, True)
